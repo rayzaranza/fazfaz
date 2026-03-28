8450bc86c4eb6e23b22e1edfe08e9957d37f4350
rayzaranza <rayzaranza@gmail.com>
2026-03-28 14:05:13 -0300

    refactor: create reusable squircle css class

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -22,6 +22,7 @@ export function Button({
         "h-10 text-lg rounded-2xl px-4 cursor-pointer gap-2 flex place-items-center",
         "shadow-elevated hover:shadow-elevated-hover active:shadow-inset",
         "hover:-translate-y-px active:translate-y-0.5 transform-gpu transition",
+        "squircle",
         variants[variant],
       )}
       {...rest}

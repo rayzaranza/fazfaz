1d7d68ae87bc6a27773b3084fbffc44242ecba83
rayzaranza <rayzaranza@gmail.com>
2026-03-28 20:24:48 -0300

    fix: adjust components styles

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -12,13 +12,13 @@ export function Card({ icon = TreePalm, title }: CardProps) {
   return (
     <div
       className={cn(
-        "squircle flex cursor-pointer flex-col gap-7 rounded-3xl p-4 pb-2.5 select-none",
+        "squircle flex min-w-36 cursor-pointer flex-col gap-7 rounded-3xl p-4 pb-3 select-none",
         "bg-surface hover:bg-surface-hover active:bg-surface-pressed",
         "shadow-elevated hover:shadow-elevated-hover active:shadow-inset",
         "transition hover:-translate-y-0.5 active:translate-y-0",
       )}
     >
-      <Icon icon={icon} className="stroke-rose-400" />
+      <Icon icon={icon} />
       <Text className="text-xl">{title}</Text>
     </div>
   );

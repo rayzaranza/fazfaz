fd35f088de431b3b2d59437eb690679bfb76ac62
rayzaranza <rayzaranza@gmail.com>
2026-03-28 05:20:17 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Button } from "./components/Button";
+import { Icon } from "./components/Icon";
+import { Plus, ArrowLeft, Trash } from "lucide-react";
 
 export function App() {
   return (
@@ -7,13 +9,36 @@ export function App() {
         <h1 className="text-6xl">cabeçalho 1</h1>
         <h2 className="text-5xl">cabeçalho 2</h2>
         <h2 className="text-4xl">cabeçalho 3</h2>
+
+        <div className="flex gap-5 place-items-center">
+          <Icon icon={Plus} size="large" />
+          <Icon icon={Plus} />
+          <Icon icon={Plus} size="small" />
+        </div>
+
+        <div className="flex gap-5 place-items-center">
+          <Icon icon={ArrowLeft} size="large" />
+          <Icon icon={ArrowLeft} />
+          <Icon icon={ArrowLeft} size="small" />
+        </div>
+
+        <div className="flex gap-5 place-items-center">
+          <Icon icon={Trash} size="large" />
+          <Icon icon={Trash} />
+          <Icon icon={Trash} size="small" />
+        </div>
+
         <p className="text-xl max-w-2xl">
           sdhai ygsdo haodhy ophs odgasd iogasdh asd gaosfc asdaisdais naaois7
           aosasdiasdaggadga asdgaysdgai agsdaisg gs sdgaa aidad agsda s
         </p>
       </div>
+
       <div className="flex gap-4">
-        <Button>botão</Button>
+        <Button>
+          <Icon icon={Plus} />
+          botão
+        </Button>
         <Button variant="accent">botão</Button>
         <Button variant="danger">botão</Button>
       </div>

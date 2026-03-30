f0b0dff2b0dc56c4b2db6288db31cfe79529f31e
rayzaranza <rayzaranza@gmail.com>
2026-03-30 09:40:14 -0300

    feat: configure supabase client with database types

---
+++ src/lib/supabase.ts
@@ -0,0 +1,7 @@
+import { type Database } from "../types/database.types";
+import { createClient } from "@supabase/supabase-js";
+
+const url = import.meta.env.VITE_SUPABASE_URL;
+const key = import.meta.env.VITE_SUPABASE_KEY;
+
+export const supabase = createClient<Database>(url, key);

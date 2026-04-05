80fdda98b8fb6475fecfed888327e7ce3ef2b1e9
Ray Zaranza <rayzaranza@gmail.com>
2026-04-05 18:13:21 -0300

    feat: complete task

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -2,38 +2,57 @@ import { Check } from "lucide-react";
 import { Text } from "./Text";
 import { cn } from "@/utils/classNames";
 import { Icon } from "./Icon";
+import { type ChangeEvent, useOptimistic, useTransition } from "react";
 
 interface TaskProps {
   name: string;
   isDone: boolean;
   className?: string;
+  onToggle: (value: boolean) => Promise<void>;
 }
 
-export function Task({ name, isDone, className }: TaskProps) {
+export function Task({ name, isDone, className, onToggle }: TaskProps) {
+  const [optimisticIsDone, addOptimisticIsDone] = useOptimistic(
+    isDone,
+    (_state, newValue: boolean) => newValue,
+  );
+
+  const [isPending, startTransition] = useTransition();
+
+  function handleCheckboxChange({ target }: ChangeEvent<HTMLInputElement>) {
+    startTransition(async () => {
+      addOptimisticIsDone(target.checked);
+      await onToggle(target.checked);
+    });
+  }
   return (
     <div
       className={cn(
         "group flex min-h-600 items-center gap-200 rounded-medium bg-container px-200 shadow-blocky",
         className,
+        isPending && "pointer-events-none",
       )}
     >
       <div className="relative flex items-center justify-center">
-        {isDone && (
+        {optimisticIsDone && (
           <Icon
             icon={Check}
             className="pointer-events-none absolute z-40 text-content-inverse"
           />
         )}
         <input
+          disabled={isPending}
           className="size-300 shrink-0 blocky-inset appearance-none rounded-full checked:bg-container-inverse checked:shadow-none"
           type="checkbox"
+          onChange={handleCheckboxChange}
+          checked={optimisticIsDone}
         />
       </div>
 
       <Text
         className={cn(
           "w-full transition-all",
-          isDone && "line-through opacity-60",
+          optimisticIsDone && "line-through opacity-60",
         )}
       >
         {name}
@@ -42,6 +61,7 @@ export function Task({ name, isDone, className }: TaskProps) {
       <div
         className={cn(
           "flex gap-100 opacity-0 focus-within:opacity-100 md:group-hover:opacity-100",
+          isPending && "opacity-100",
         )}
       ></div>
     </div>
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,16 +1,23 @@
 import type { TaskListItem } from "@/types/tasks";
 import { Task } from "./Task";
+import { useUpdateTask } from "@/hooks/useUpdateTask";
 
 interface TaskListProps {
   tasks: TaskListItem[];
 }
 
 export function TaskList({ tasks }: TaskListProps) {
+  const { handleToggle } = useUpdateTask();
+
   return (
     <ul className="flex flex-col gap-200">
       {tasks.map(({ name, id, is_done }) => (
         <li key={id}>
-          <Task name={name} isDone={is_done} />
+          <Task
+            name={name}
+            isDone={is_done}
+            onToggle={(value) => handleToggle(id, value)}
+          />
         </li>
       ))}
     </ul>

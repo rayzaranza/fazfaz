85313fa1d8ce36a81e15a613cd6fbdf7644224fb
rayzaranza <rayzaranza@gmail.com>
2026-04-05 15:51:04 -0300

    fix: remove features unrelated to creation from Task and TaskList

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,68 +1,39 @@
-import { Check, Trash2 } from "lucide-react";
-import { Button } from "./Button";
+import { Check } from "lucide-react";
 import { Text } from "./Text";
 import { cn } from "@/utils/classNames";
 import { Icon } from "./Icon";
-import { type ChangeEvent, useOptimistic, useTransition } from "react";
 
 interface TaskProps {
   name: string;
   isDone: boolean;
   className?: string;
-  isDeleting?: boolean;
-  onDelete: () => void;
-  onChange: (isDone: boolean) => Promise<void>;
 }
 
-export function Task({
-  name,
-  isDone,
-  className,
-  onDelete,
-  isDeleting,
-  onChange,
-}: TaskProps) {
-  const [optimisticIsDone, addOptimisticIsDone] = useOptimistic(
-    isDone,
-    (_state, newValue: boolean) => newValue,
-  );
-  const [isPending, startTransition] = useTransition();
-
-  function handleCheckboxChange({ target }: ChangeEvent<HTMLInputElement>) {
-    startTransition(async () => {
-      addOptimisticIsDone(target.checked);
-      await onChange(target.checked);
-    });
-  }
-
+export function Task({ name, isDone, className }: TaskProps) {
   return (
     <div
       className={cn(
         "group flex min-h-600 items-center gap-200 rounded-medium bg-container px-200 shadow-blocky",
         className,
-        (isDeleting || isPending) && "pointer-events-none",
       )}
     >
       <div className="relative flex items-center justify-center">
-        {optimisticIsDone && (
+        {isDone && (
           <Icon
             icon={Check}
             className="pointer-events-none absolute z-40 text-content-inverse"
           />
         )}
         <input
-          disabled={isPending || isDeleting}
           className="size-300 shrink-0 blocky-inset appearance-none rounded-full checked:bg-container-inverse checked:shadow-none"
           type="checkbox"
-          onChange={handleCheckboxChange}
-          checked={optimisticIsDone}
         />
       </div>
 
       <Text
         className={cn(
           "w-full transition-all",
-          optimisticIsDone && "line-through opacity-60",
+          isDone && "line-through opacity-60",
         )}
       >
         {name}
@@ -71,17 +42,8 @@ export function Task({
       <div
         className={cn(
           "flex gap-100 opacity-0 focus-within:opacity-100 md:group-hover:opacity-100",
-          isDeleting && "opacity-100",
         )}
-      >
-        <Button
-          title="excluir tarefa"
-          variant="danger"
-          isLoading={isDeleting}
-          onClick={onDelete}
-          icon={Trash2}
-        />
-      </div>
+      ></div>
     </div>
   );
 }
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,4 +1,3 @@
-import { useTaskActions } from "@/hooks/useTaskActions";
 import type { TaskListItem } from "@/types/tasks";
 import { Task } from "./Task";
 
@@ -7,17 +6,11 @@ interface TaskListProps {
 }
 
 export function TaskList({ tasks }: TaskListProps) {
-  const { handleDelete, handleToggle } = useTaskActions();
   return (
     <ul className="flex flex-col gap-200">
       {tasks.map(({ name, id, is_done }) => (
         <li key={id}>
-          <Task
-            name={name}
-            isDone={is_done}
-            onDelete={() => handleDelete(id)}
-            onChange={(value) => handleToggle(id, value)}
-          />
+          <Task name={name} isDone={is_done} />
         </li>
       ))}
     </ul>
